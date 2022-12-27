a3bb5528d6babd30080f5dbd076a0965af7421f8
wasifalam23 <wsfalam84@gmail.com>
2022-12-27 22:10:35 +0530

    added cors

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const { promisify } = require('util');
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(morgan('dev'));
 
